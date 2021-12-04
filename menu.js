ac271e64fd95f655a68c0e4e0906de29c2a2e8bd
Akanowo <ukoakanowo98@gmail.com>
2021-12-04 01:15:26 +0100

    added CON to option

--- menu.js
+++ menu.js
@@ -146,7 +146,7 @@ class Menu {
 
 			utils.sendResponse(
 				res,
-				'Select a bank \n' +
+				'CON Select a bank \n' +
 					text +
 					`${endIndex >= banks.length ? '' : '97. Next\n99. Go To Main Menu'}`
 			);
@@ -174,7 +174,7 @@ class Menu {
 				for (let i = 0; i < banksToShow.length; i++) {
 					// get index of bank
 					const bankIndex = banks.findIndex((x) => x.name === banksToShow[i]);
-					text += `${bankIndex + 1} ${banksToShow[i]}\n`;
+					text += `CON ${bankIndex + 1} ${banksToShow[i]}\n`;
 				}
 
 				utils.sendResponse(
